caec9464d0423de60403d2d267812fca2bf72e5c
aurélien JANGOT <aurelien.jangot@gmail.com>
2022-02-09 21:54:00 +0100

    clean repo

--- App/connection.js
+++ App/connection.js
@@ -1,17 +1,15 @@
-import { log } from "./battleMarket.js";
-
 const startConnection = (
   provider,
   onDisconnect,
   expectedPongBack = 15000,
-  checkInterval = 7500,
+  checkInterval = 7500
 ) => {
-  let pingTimeout = null;
-  let keepAliveInterval = null;
+  let pingTimeout = null
+  let keepAliveInterval = null
 
   provider._websocket.on('open', () => {
     keepAliveInterval = setInterval(() => {
-      provider._websocket.ping();
+      provider._websocket.ping()
       // log.debug('ping');
 
       // Use `WebSocket#terminate()`, which immediately destroys the connection,
@@ -19,21 +17,21 @@ const startConnection = (
       // Delay should be equal to the interval at which your server
       // sends out pings plus a conservative assumption of the latency.
       pingTimeout = setTimeout(() => {
-        provider._websocket.terminate();
-      }, expectedPongBack);
-    }, checkInterval);
-  });
+        provider._websocket.terminate()
+      }, expectedPongBack)
+    }, checkInterval)
+  })
 
   provider._websocket.on('close', (err) => {
-    if (keepAliveInterval) clearInterval(keepAliveInterval);
-    if (pingTimeout) clearTimeout(pingTimeout);
-    onDisconnect(err);
-  });
+    if (keepAliveInterval) clearInterval(keepAliveInterval)
+    if (pingTimeout) clearTimeout(pingTimeout)
+    onDisconnect(err)
+  })
 
   provider._websocket.on('pong', () => {
     // log.debug('pong');
-    if (pingTimeout) clearInterval(pingTimeout);
-  });
-};
+    if (pingTimeout) clearInterval(pingTimeout)
+  })
+}
 
-export default startConnection;
+export default startConnection
--- App/logger.js
+++ App/logger.js
@@ -1,32 +1,32 @@
 /* eslint-disable no-underscore-dangle */
-import chalk from 'chalk';
+import chalk from 'chalk'
 
 class Logger {
-  constructor(name) {
-    this.name = name;
+  constructor (name) {
+    this.name = name
   }
 
-  _log(logLevel, args) {
-    const date = new Date();
-    const dateText = date.toTimeString().split(' ')[0];
-    console.log(`(${dateText})#${this.name}[${logLevel}]: ${args}`);
+  _log (logLevel, args) {
+    const date = new Date()
+    const dateText = date.toTimeString().split(' ')[0]
+    console.log(`(${dateText})#${this.name}[${logLevel}]: ${args}`)
   }
 
-  info(args) {
-    this._log('info', args);
+  info (args) {
+    this._log('info', args)
   }
 
-  warn(args) {
-    this._log('⚠', chalk.yellow(args));
+  warn (args) {
+    this._log('⚠', chalk.yellow(args))
   }
 
-  debug(args) {
-    this._log('🔸', chalk.blue(args));
+  debug (args) {
+    this._log('🔸', chalk.blue(args))
   }
 
-  error(args) {
-    this._log('🛑', chalk.red(args));
+  error (args) {
+    this._log('🛑', chalk.red(args))
   }
 }
 
-export default Logger;
+export default Logger
